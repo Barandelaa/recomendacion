57189844598cf65e3b2ce1621a2418481838f8a4
deibis4211 <david4211@gmail.com>
2024-11-01 13:20:39 +0100

    prueba inicial

--- review.py
+++ review.py
@@ -1,1 +1,18 @@
-class Review:
+import pandas as pd
+
+def obtener_datos():
+    df = pd.read_csv('Processed_interactions.csv')
+    return df
+
+def obtener_nombre_receta(id_receta):
+    df = pd.read_csv('Processed_recipes.csv')
+
+    if df is not None:
+        receta = df[df['id'] == id_receta]
+        if not receta.empty:
+            return receta['name'].values[0]
+        else:
+            print(f"La receta con id {id_receta} no se encontró.")
+            return None
+
+print(obtener_nombre_receta(277748))
